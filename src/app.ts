94849136165f1cbcb8811636a2319970eb53346a
Matteo Depascale <m.depascale@reply.it>
2023-02-22 07:33:30 +0100

    feat: enable ssr

--- src/app.ts
+++
@@ -1,3 +0,0 @@
-import { Amplify } from 'aws-amplify';
-import awsmobile from './aws-exports';
-Amplify.configure(awsmobile);
--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -5,7 +5,7 @@ const awsmobile = {
     "aws_project_region": "REGION",
     "aws_cognito_region": "REGION",
     "aws_user_pools_id": "USER_POOL_ID",
-  "aws_user_pools_web_client_id": "USER_POOL_CLIENT_ID",
+    "aws_user_pools_web_client_id": "USER_POOL_CLIENT_ID",
     "oauth": {
       "domain": "USER_POOL_DOMAIN.auth.REGION.amazoncognito.com"
     },
