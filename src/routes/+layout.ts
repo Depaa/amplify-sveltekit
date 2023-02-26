30b2de3fe4eff7edc037bed0a3bf25d5a1571357
Depaa <depascale.matteo@gmail.com>
2023-02-26 14:45:33 +0100

    feat: move amplify configure

---
+++ src/routes/+layout.ts
@@ -0,0 +1,3 @@
+import { Amplify } from 'aws-amplify';
+import awsmobile from '../aws-exports';
+Amplify.configure(awsmobile);
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,6 @@ import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
-	//https://github.com/aws-amplify/amplify-js/issues/9639#issuecomment-1271856620
-	define: {
-		global: {} //fix dev build
-	},
 	resolve: {
 		alias: {
 			'./runtimeConfig': './runtimeConfig.browser' //fix production build
